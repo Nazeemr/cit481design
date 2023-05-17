4d5130b2dfd51066bf8ac4114ee4ef2cf256c7b8
Daniel Y Cheon <77998304+spantoport@users.noreply.github.com>
2023-05-17 02:33:27 -0700

    sign_up.php

--- sign_up.php
+++ sign_up.php
@@ -1,6 +1,6 @@
 <?php
 
-include("senior_database_connect.php"); 
+$db = mysqli_connect('testdata2-instance-1.crxvoye8t6bs.us-west-1.rds.amazonaws.com', 'admin', 'password','project'); 
 
 
 if (isset($_POST['sign_up'])) {
