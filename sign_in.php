c45128cfb983ecf4faade293f9e453f52436d33f
Daniel Y Cheon <77998304+spantoport@users.noreply.github.com>
2023-05-17 01:45:42 -0700

    sign_in.php

--- sign_in.php
+++ sign_in.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', 'dnflwlq88','project');
+$db = mysqli_connect('testdata2-instance-1.crxvoye8t6bs.us-west-1.rds.amazonaws.com', 'admin', 'password','project');
 
 
 
